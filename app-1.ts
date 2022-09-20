24efb71fc9f25644a21155c13c6d40ccecf78239
viry <vbatten10@hotmail.com>
2022-09-20 13:13:01 -0500

    Configurando más acciones en reducer

--- app-1.ts
+++ app-1.ts
@@ -9,16 +9,47 @@ interface Action {
 // }
 
 function reducer(state = 10, action: Action) {
-    if (action.type === 'INCREMENTAR') {
-        return state += 1;
-    }
-    return state;
-}
+    // if (action.type === 'INCREMENTAR') {
+    //     return state += 1;
+    // }
 
+    switch(action.type){
+        case 'INCREMENTAR': 
+            return state += 1;
+        case 'DECREMENTAR': 
+            return state -= 1;
+        case 'MULTIPLICAR':
+            return state * action.payload;
+        case 'DIVIDIR':
+            return state / action.payload;
+        default:
+            return state;
+    }
+}
 
 // Usar el reducer
 const incrementadorAction: Action = {
     type: 'INCREMENTAR'
 }
 
+const decrementadorAction: Action = {
+    type: 'DECREMENTAR'
+}
+
+const multiplicarAction: Action = {
+    type: 'MULTIPLICAR',
+    payload: 2
+}
+
+const dividirAction: Action = {
+    type: 'DIVIDIR',
+    payload: 2
+}
+
 console.log(reducer(10, incrementadorAction));
+
+console.log(reducer(10, decrementadorAction));
+
+console.log(reducer(10, multiplicarAction));
+
+console.log(reducer(10, dividirAction));
