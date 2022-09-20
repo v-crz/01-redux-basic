89b8059d554eee32fa67a72dc34aa4176f687733
viry <vbatten10@hotmail.com>
2022-09-20 12:43:44 -0500

    Acciones y reducer básico

--- app-1.ts
+++ app-1.ts
@@ -1,1 +1,24 @@
-console.log('Hola mundo');
+// Acciones
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+// const incrementadorAction: Action = {
+//     type: 'INCREMENTAR'
+// }
+
+function reducer(state = 10, action: Action) {
+    if (action.type === 'INCREMENTAR') {
+        return state += 1;
+    }
+    return state;
+}
+
+
+// Usar el reducer
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+}
+
+console.log(reducer(10, incrementadorAction));
